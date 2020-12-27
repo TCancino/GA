a78f1aadc2ccce74ec5d27ea255d0d7972f6c497
RicardoAndres30 <ricaardo534@hotmail.com>
2020-12-27 05:17:38 -0300

    Terminado. Es capaz de generar todos los horarios posibles. Falta ingresar restricciones de ventanas

--- backtracking_script.py
+++ backtracking_script.py
@@ -1,22 +1,3 @@
-#M = [ 
-#    
-#    [ [2,0,2,0,3] , [3,0,3,0,2] ], #Ramo 1 y sus secciones
-#    [ [1,0,1,0,1] , [2,0,2,0,2] ], #Ramo 2 y sus secciones
-#    [ [0,3,4,0,3] , [0,4,3,0,4] ], #Ramo 3 y sus secciones
-#    [ [2,0,3,0,2] , [0,6,0,6,6] ], #Ramo 4 y sus secciones
-#
-#    ]
-
-#M = [ 
-#    
-#    [ [2,0,2,0,3] ], #Ramo 1 y sus secciones
-#    [ [1,0,1,0,1] , [4,0,4,0,4] ] #Ramo 2 y sus secciones
-#
-#    ]
-
-V = []
-S = []
-
 def checking (S, options, i_course, i_section):
     check = True
     for section in range(len(S)):
@@ -32,23 +13,13 @@ def backtracking (options, i_course, i_section, S):
     
     if i_course == len(M): #Revisión si quedan más ramos por revisar
         print(S)
-        #if S not in V:
-        V.append(S)
-            #V.append(S)
-            #print(S)
-            #V.append(S)
-            #V.append("Otro horario")
-            #print(V)
         return S
 
     elif i_section == len(options[i_course]): #Revisión si quedan más secciones por revisar
-        #print(S)
         return S
 
     else:
-
         if len(S) == 0:
-            #S.append(options[i_course][i_section])
             backtracking(options, i_course, i_section + 1, [])
 
         else:
@@ -57,14 +28,28 @@ def backtracking (options, i_course, i_section, S):
         if checking(S, options, i_course, i_section): #Se revisa compatibilidad
             S.append(options[i_course][i_section])
             #print(S)
-            #if S not in V:
-            #    V.append(S)
             backtracking(options, i_course + 1, 0, S)
-        
-        #else:
-        #    backtracking(options, i_course, i_section + 1, [])
 
-backtracking(M,0,0,S)
+            S.pop()
+
+#M = [ 
+#    
+#    [ [2,0,2,0,3] , [3,0,3,0,2] ], #Ramo 1 y sus secciones
+#    [ [1,0,1,0,1] , [7,0,7,0,7] ], #Ramo 2 y sus secciones
+#    [ [0,3,4,0,3] , [0,4,3,0,4] ], #Ramo 3 y sus secciones
+#    [ [2,0,3,0,2] , [0,6,0,6,6] ], #Ramo 4 y sus secciones
+#
+#    ]
+
+M = [ 
+    
+    [ [1,0,1,0,1] , [2,0,2,0,2], [3,0,3,0,3] ], #Ramo 1 y sus secciones
+    [ [4,0,4,0,4] , [5,0,5,0,5], [6,0,6,0,6] ], #Ramo 2 y sus secciones
+    [ [7,0,7,0,7] , [8,0,8,0,8], [9,0,9,0,9] ]
+
+    ]
+
+backtracking(M,0,0,[])
 #for s in V:
 #    print(s)
 #print(V)
